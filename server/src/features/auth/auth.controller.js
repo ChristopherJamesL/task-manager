f9e0034399fa79bda5a4b0d7b736e413dc1ed1d4
ChristopherL <anbchris@gmail.com>
2026-06-09 14:35:40 -0400

    fix suspected cookie error

--- server/src/features/auth/auth.controller.js
+++ server/src/features/auth/auth.controller.js
@@ -19,9 +19,9 @@ async function httpSignIn(req, res) {
 
   res.cookie("sid", sessionId, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV === "production",
-    // sameSite: "lax",
-    secure: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    // secure: true,
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/",
